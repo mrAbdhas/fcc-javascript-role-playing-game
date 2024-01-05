8d0846233f54bb84b7eb76f8d4a9b4f1b4c884ec
mrAbdhas <143132916+mrAbdhas@users.noreply.github.com>
2024-01-05 11:02:33 +0100

    step61 - step80

--- script.js
+++ script.js
@@ -31,6 +31,12 @@ const locations = [
         "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
         "button functions": [buyHealth, buyWeapon, goTown],
         text: "You enter the store."
+    },
+    {
+        name: "cave",
+        "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+        "button functions": [fightSlime, fightBeast, goTown],
+        text: "You enter the cave. You see some monsters."
     }
 ];
 // initialize buttons
@@ -40,32 +46,41 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 // 
+/* reusabæe fumction, instead of assigning innerText and onclick
+to specific strings and functions, and aviod repttions we use
+this update funtions. and call it where is needed*/ 
 function update(location){
-    button1.innerText = "Go to store"
-    button2.innerText = "Go to cave"
-    button3.innerText = "Fight dragon"
+    /*updating  button1-3 text to be assigned to 
+    the first,secound and third value of the button text array */
+    button1.innerText = location["button text"][0]; //location["button text"] is an array with three elements, [0] for the first element of the array
+    button2.innerText = location["button text"][1];
+    button3.innerText = location["button text"][2];
     
-    // updating the onclick property for each button 
-    button1.onclick = goStore;
-    button2.onclick = goCave;
-    button3.onclick = fightDragon;
+    /* updating the onclick property for each button
+    to be assigned to the first,second and third value of the 
+    button functions array*/ 
+    button1.onclick = location["button functions"][0];
+    button2.onclick = location["button functions"][1];
+    button3.onclick = location["button functions"][2];
     
-    // modifying display text for onclick event
-    text.innerText = "You are in the town square. You see a sign that says \"Store\"."
+    /* updating the text.innerText to be assigned to the text
+    from the location object*/ 
+    text.innerText = location.text;
 
 }
 
 function goTown() {
-
+    update(locations[0]);
 
 }
 
 function goStore() {
+    update(locations[1]);
 
 }
 
 function goCave() {
-    console.log("Going to cave.");
+    update(locations[2]);
 }
 
 function fightDragon(){
@@ -73,9 +88,27 @@ function fightDragon(){
 }
 
 function buyHealth() {
+    if(gold >= 10){
+        gold: gold -= 10;
+        health: health += 10;
+    
+        goldText.innerText = gold;
+        healthText.innerText = health;
 
+    }
+    else{
+        text.innerText = "You do not have enough gold to buy health."
+    }
 }
 
 function buyWeapon() {
+
+}
+
+function fightSlime() {
+
+}
+
+function fightBeast() {
 
 }
